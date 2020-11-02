4243e25e3326fce6c70b71ab80674e3bc59780c0
Ben Hamelin <bhamelin@oomphinc.com>
2020-11-02 10:03:13 -0500

    Add config directory and post settings hook

---
+++ factory-hooks/post-settings-php/config.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ *  Set vcs and sync dirs to writable directories above docroot.
+ */
+$config_directories['vcs'] = '../config/default';
+$config_directories['sync'] = '../config/default';
