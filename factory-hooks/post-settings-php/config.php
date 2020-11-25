751f44f22e840dd72fc4b19934e9f11e3857251c
Ben Hamelin <bhamelin@oomphinc.com>
2020-11-25 09:29:05 -0500

    Change paths

--- factory-hooks/post-settings-php/config.php
+++ factory-hooks/post-settings-php/config.php
@@ -6,5 +6,5 @@ declare(strict_types = 1);
  *  Set vcs and sync dirs to writable directories above docroot.
  */
 
-$settings['config_sync_directory'] = '../../../config/default';
-$settings['config_vcs_directory'] = '../../../config/default';
+$settings['config_sync_directory'] = '../../config/default';
+$settings['config_vcs_directory'] = '../../config/default';
