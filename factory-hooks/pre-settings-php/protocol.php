b6ce50fe20e09af1d48f1cb29e70e437ea6452f7
Philip Frilling <pfrilling@oomphinc.com>
2020-12-08 11:05:53 -0500

    Site Factory install error for new sites (#53)
    * Added a function exists method
    
    * Updated the changelog

--- factory-hooks/pre-settings-php/protocol.php
+++ factory-hooks/pre-settings-php/protocol.php
@@ -2,19 +2,22 @@
 
 declare(strict_types = 1);
 
-/**
- * Test if the current request is encrypted.
- * @return bool
- */
-function isSecure(): bool {
-  $isSecure = FALSE;
-  if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
-    $isSecure = TRUE;
+// Check if the isSecure function exists before generating it.
+if (!function_exists('isSecure')) {
+  /**
+   * Test if the current request is encrypted.
+   * @return bool
+   */
+  function isSecure(): bool {
+    $isSecure = FALSE;
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+      $isSecure = TRUE;
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] === 'on') {
+      $isSecure = TRUE;
+    }
+    return $isSecure;
   }
-  elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] === 'on') {
-    $isSecure = TRUE;
-  }
-  return $isSecure;
 }
 
 // Redirect to the secure version.
