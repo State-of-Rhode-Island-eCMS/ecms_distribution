fcec82141bf6dab66e0176eb551661612a720a7b
Ben Hamelin <bhamelin@oomphinc.com>
2021-01-27 12:04:20 -0500

    Solr core override

---
+++ factory-hooks/post-settings-php/solr-core-override.php
@@ -0,0 +1,6 @@
+<?php
+
+declare(strict_types = 1);
+
+// Override Acquia Search Solr search core.
+$config['acquia_search_solr.settings']['override_search_core'] = "AMTW-199087.{$_ENV['AH_SITE_ENVIRONMENT']}.riecms";
