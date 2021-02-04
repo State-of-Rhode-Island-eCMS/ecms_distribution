00caacf536e57460b216c3282de5d77d3c52c8bf
Ben Hamelin <bhamelin@oomphinc.com>
2021-02-04 14:45:49 -0500

    Update solr server environment override

--- factory-hooks/post-settings-php/solr-core-override.php
+++ factory-hooks/post-settings-php/solr-core-override.php
@@ -3,4 +3,17 @@
 declare(strict_types = 1);
 
 // Override Acquia Search Solr search core.
-$config['acquia_search_solr.settings']['override_search_core'] = "AMTW-199087.01dev.riecms";
+if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
+  switch ($_ENV['AH_SITE_ENVIRONMENT']) {
+    case '01test':
+      $config['acquia_search_solr.settings']['override_search_core'] = "AMTW-199087.01test.riecms";
+      break;
+    case '01live':
+      $config['acquia_search_solr.settings']['override_search_core'] = "AMTW-199087.01live.riecms";
+      break;
+    default:
+      // Use the 01dev server for all others
+      $config['acquia_search_solr.settings']['override_search_core'] = "AMTW-199087.01dev.riecms";
+      break;
+  }
+}
