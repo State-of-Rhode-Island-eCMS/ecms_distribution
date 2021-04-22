2940377a73520763ad8cb7a56e41da4a955860a6
Ben Hamelin <bhamelin@oomphinc.com>
2021-04-22 15:50:34 -0400

    Update solr core override settings

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ modified Semantic Versioning scheme. See the "Versioning scheme" section of the
 ### Added
 
 ### Changed
+- RIGA-35: Update solr override settings.
 
 ### Deprecated
 
--- factory-hooks/post-settings-php/solr-core-override.php
+++ factory-hooks/post-settings-php/solr-core-override.php
@@ -6,14 +6,14 @@ declare(strict_types = 1);
 if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
   switch ($_ENV['AH_SITE_ENVIRONMENT']) {
     case '01test':
-      $config['acquia_search_solr.settings']['override_search_core'] = "AMTW-199087.01test.riecms";
+      $config['acquia_search.settings']['override_search_core'] = "AMTW-199087.01test.riecms";
       break;
     case '01live':
-      $config['acquia_search_solr.settings']['override_search_core'] = "AMTW-199087.01live.riecms";
+      $config['acquia_search.settings']['override_search_core'] = "AMTW-199087.01live.riecms";
       break;
     default:
       // Use the 01dev server for all others
-      $config['acquia_search_solr.settings']['override_search_core'] = "AMTW-199087.01dev.riecms";
+      $config['acquia_search.settings']['override_search_core'] = "AMTW-199087.01dev.riecms";
       break;
   }
 }
