255654e80c291b88e29e86691f683fa675b59127
swiftwinning <jwinning@oomphinc.com>
2023-03-30 12:58:42 -0400

    Add post settings hook

---
+++ factory-hooks/post-settings-php/search-settings.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Allow customizable search config on site-by-site basis.
+ */
+
+//$char_limit = \Drupal::config('secms_search_settings')->get('char_limit');
+//
+//$config['name-of-search-module-config']['name-of-character-limit-config-setting'] = $char_limit;
