4c749f654d1f174460a1c4ed9aae74b732f710cf
Phil Frilling <pfrilling@oomphinc.com>
2024-08-26 15:13:11 -0400

    Updated the secrets location

--- factory-hooks/post-settings-php/secrets.php
+++ factory-hooks/post-settings-php/secrets.php
@@ -7,3 +7,9 @@ $secrets_file = sprintf('/mnt/files/%s.%s/secrets.settings.php', $_ENV['AH_SITE_
 if (file_exists($secrets_file)) {
   require $secrets_file;
 }
+
+$cloudNextSecretsFile = sprintf('%s/secrets.settings.php', $_ENV['HOME']);
+
+if (file_exists($cloudNextSecretsFile)) {
+  require $cloudNextSecretsFile;
+}
