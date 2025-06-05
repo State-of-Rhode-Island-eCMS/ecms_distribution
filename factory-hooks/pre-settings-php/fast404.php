3979ddfb1859d6c402510f89224cb71868219e75
Junia Biswas <jbiswas@oomphinc.com>
2025-06-05 10:42:38 -0700

    add settings

--- factory-hooks/pre-settings-php/fast404.php
+++ factory-hooks/pre-settings-php/fast404.php
@@ -1,10 +1,12 @@
 <?php
 
-
 /**
  * @file
  * Add settings for the Fast 404 module.
  */
 
- $settings['fast404_exts'] =
-'/^(?!\/robots)^(?!\/system\/files).*\.(txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
+$settings['fast_404'] = [
+  'exclude_paths' => '/\/(?:styles)|(?:system\/files)\//',
+  'paths' => '/\.(txt|png|gif|jpg|jpeg|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i',
+  'html' => '<!DOCTYPE html><html><head><title>404 Not Found</title></head><body><h1>Not Found</h1></body></html>',
+];
